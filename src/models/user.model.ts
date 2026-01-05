4bffe9a2344c03a1259863d748ae37f049463b26
Aryan <aaryan.nakarmi111@gmail.com>
2026-01-05 11:00:04 +0545

    user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { UserType } from "../types/user.type";
+const UserSchema: Schema = new Schema<UserType>(
+    {
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        username: { type: String, required: true, unique: true },
+        fullName: { type: String },
+        role: {
+            type: String,
+            enum: ['user', 'admin'],
+            default: 'user',
+        }
+    },
+    {
+        timestamps: true, // auto createdAt and updatedAt
+    }
+);
+
+export interface IUser extends UserType, Document { 
+    // combine UserType and Document
+    // mongo related attribute/ custom attributes
+    _id: mongoose.Types.ObjectId; 
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export const UserModel = mongoose.model<IUser>('User', UserSchema);
+// UserModel is the mongoose model for User collection
+// db.users in MongoDB
