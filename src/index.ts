df3ec172fa36889cd0bbfc90754093258e0f4bf5
Aryan <aaryan.nakarmi111@gmail.com>
2026-01-05 11:31:42 +0545

    index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express, { Application, Request, Response} from 'express';
 import bodyParser from 'body-parser';
-
 import { connectDatabase } from './database/mongodb';
 import { PORT } from './config';
 import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.route";
+
 
 dotenv.config();
 //can use .env variable below this
@@ -14,16 +15,10 @@ const app: Application = express();
 
 app.use(bodyParser.json());
 
-
-import authRoutes from "./routes/auth.route";
-
-
 app.use("/api/auth",authRoutes);
 
-
-
 app.get('/', (req:Request, res:Response) =>{
-    res.send('Hello, World!');
+      return res.status(200).json({ success: "true", message: "Welcome to the API" });
 });
 
 async function startServer() {
