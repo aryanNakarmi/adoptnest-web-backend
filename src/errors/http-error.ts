de141a18d3c985473bf01c9e3c41abdc609dbbd0
Aryan <aaryan.nakarmi111@gmail.com>
2026-01-05 10:48:47 +0545

    Http Errors

--- src/errors/http-error.ts
+++ src/errors/http-error.ts
@@ -1,7 +1,3 @@
-export class HttpError extends Error {
-    statusCode: number
-    constructor(statusCode: number, message: string) {
-        super(message);
-        this.statusCode = statusCode;
-    }
+export class HttpError extends Error{
+    
 }
