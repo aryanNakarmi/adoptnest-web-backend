e63b2f9a4dd3ff7cfe93ab01ce2c6b8b7f179f59
Aryan <aaryan.nakarmi111@gmail.com>
2026-02-01 09:03:11 +0545

    admin router

---
+++ src/routes/admin.route.ts
@@ -0,0 +1,15 @@
+import express, { Router } from 'express';
+import { AdminController } from '../controllers/admin.controller';
+import { protect, adminMiddleware } from '../middleware/authorized.middleware';
+import { uploadImage } from '../middleware/multer.middleware';
+
+const router: Router = express.Router();
+const controller = new AdminController();
+
+router.post('/users', protect, adminMiddleware, uploadImage.single('profilePicture'), controller.createUser);
+router.get('/users', protect, adminMiddleware, controller.getAllUsers);
+router.get('/users/:id', protect, adminMiddleware, controller.getUserById);
+router.put('/users/:id', protect, adminMiddleware, uploadImage.single('profilePicture'), controller.updateUser);
+router.delete('/users/:id', protect, adminMiddleware, controller.deleteUser);
+
+export default router;
